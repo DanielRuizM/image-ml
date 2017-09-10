bb97877c1d56c3aebadb8381839a653b2b2970f8
danielruizm <danielrumayo@hotmail.com>
2017-09-10 16:55:48 +0200

    predictions completo

--- predictions_loop.py
+++ predictions_loop.py
@@ -4,6 +4,7 @@ import sys
 
 
 def main():
+    
     if len(sys.argv) >3:
         print("""
         You are introducing wrong parameters, you need:
@@ -20,15 +21,18 @@ def main():
         filename_path=jpges_path+'/'+filename
         os.system("{} predict_images.py {} {} 2> /dev/null".format(python_path,folder_path,filename_path))
 
+
     count_lines=0
     correct=0
+
     with open(folder_path+'predictions.csv','r') as predictions:
-        count_lines=count_lines+1
         for line in predictions: 
+            count_lines=count_lines+1
             result=line.split(',')[-1]
             correct=correct+int(result)
-
-    print("The accuracy of this algorithm is "+((correct/count_lines)*100)+"%")
+    print(count_lines)
+    print(correct)
+    print("The accuracy of this algorithm is "+str(((correct/count_lines)*100))+"%")
 
 
 if __name__ == "__main__":
